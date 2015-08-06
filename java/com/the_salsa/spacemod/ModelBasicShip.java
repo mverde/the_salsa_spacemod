a04c3a161bffdfcf2f5e3c078a6583574dff5655
Martin Verde <martinthespartan23@gmail.com>
2015-08-06 10:43:54 -0700

    Added better model and texture to ship

--- java/com/the_salsa/spacemod/ModelBasicShip.java
+++ java/com/the_salsa/spacemod/ModelBasicShip.java
@@ -1,5 +1,7 @@
 package com.the_salsa.spacemod;
 
+import org.lwjgl.opengl.GL11;
+
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
@@ -10,7 +12,9 @@ public class ModelBasicShip extends ModelBase
     ModelRenderer chassisMain;
     ModelRenderer chassisBody;
     ModelRenderer engineRight;
+    ModelRenderer engineTipRight;
     ModelRenderer engineLeft;
+    ModelRenderer engineTipLeft;
     ModelRenderer wingLeft;
     ModelRenderer wingRight;
     ModelRenderer chassisDetailTop;
@@ -26,9 +30,8 @@ public class ModelBasicShip extends ModelBase
     ModelRenderer cockpitRight;
     ModelRenderer cockpitLeft;
     ModelRenderer cockpitFront;
+    ModelRenderer cockpitBack;
     ModelRenderer topFin;
-    ModelRenderer engineTipLeft;
-    ModelRenderer engineTipRight;
   
   public ModelBasicShip()
   {
@@ -53,12 +56,24 @@ public class ModelBasicShip extends ModelBase
       engineRight.setTextureSize(64, 32);
       engineRight.mirror = true;
       setRotation(engineRight, 0F, 3.141593F, 0F);
+      engineTipRight = new ModelRenderer(this, 26, 209);
+      engineTipRight.addBox(9.5F, -8.5F, -18.06667F, 9, 9, 3);
+      engineTipRight.setRotationPoint(0F, 12F, 0F);
+      engineTipRight.setTextureSize(256, 256);
+      engineTipRight.mirror = true;
+      setRotation(engineTipRight, 0F, 3.141593F, 0F);
       engineLeft = new ModelRenderer(this, 63, 70);
       engineLeft.addBox(-18F, -8F, -17F, 8, 8, 19);
       engineLeft.setRotationPoint(0F, 12F, 0F);
       engineLeft.setTextureSize(64, 32);
       engineLeft.mirror = true;
       setRotation(engineLeft, 0F, 3.141593F, 0F);
+      engineTipLeft = new ModelRenderer(this, 0, 209);
+      engineTipLeft.addBox(-18.5F, -8.5F, -19F, 9, 9, 3);
+      engineTipLeft.setRotationPoint(0F, 12F, 0F);
+      engineTipLeft.setTextureSize(256, 256);
+      engineTipLeft.mirror = true;
+      setRotation(engineTipLeft, 0F, 3.141593F, 0F);
       wingLeft = new ModelRenderer(this, 0, 102);
       wingLeft.addBox(-37F, 0F, -6F, 28, 1, 16);
       wingLeft.setRotationPoint(0F, 12F, 0F);
@@ -126,47 +141,41 @@ public class ModelBasicShip extends ModelBase
       chassisBack.mirror = true;
       setRotation(chassisBack, 0F, 3.141593F, 0F);
       cockpitTop = new ModelRenderer(this, 155, 0);
-      cockpitTop.addBox(-4F, -12F, -4F, 8, 0, 14);
+      cockpitTop.addBox(-6F, -14F, -4F, 12, 0, 14);
       cockpitTop.setRotationPoint(0F, 12F, 0F);
       cockpitTop.setTextureSize(256, 256);
       cockpitTop.mirror = true;
       setRotation(cockpitTop, 0F, 3.141593F, 0F);
-      cockpitRight = new ModelRenderer(this, 155, 14);
-      cockpitRight.addBox(4F, -12F, -4F, 0, 7, 14);
+      cockpitRight = new ModelRenderer(this, 156, 16);
+      cockpitRight.addBox(6F, -14F, -4F, 0, 9, 14);
       cockpitRight.setRotationPoint(0F, 12F, 0F);
       cockpitRight.setTextureSize(256, 256);
       cockpitRight.mirror = true;
       setRotation(cockpitRight, 0F, 3.141593F, 0F);
-      cockpitLeft = new ModelRenderer(this, 181, 14);
-      cockpitLeft.addBox(-4F, -12F, -4F, 0, 7, 14);
+      cockpitLeft = new ModelRenderer(this, 186, 16);
+      cockpitLeft.addBox(-6F, -14F, -4F, 0, 9, 14);
       cockpitLeft.setRotationPoint(0F, 12F, 0F);
       cockpitLeft.setTextureSize(256, 256);
       cockpitLeft.mirror = true;
       setRotation(cockpitLeft, 0F, 3.141593F, 0F);
-      cockpitFront = new ModelRenderer(this, 155, 34);
-      cockpitFront.addBox(-4F, -12F, 10F, 8, 7, 0);
+      cockpitFront = new ModelRenderer(this, 155, 47);
+      cockpitFront.addBox(-6F, -14F, 10F, 12, 9, 0);
       cockpitFront.setRotationPoint(0F, 12F, 0F);
       cockpitFront.setTextureSize(256, 256);
       cockpitFront.mirror = true;
       setRotation(cockpitFront, 0F, 3.141593F, 0F);
+      cockpitBack = new ModelRenderer(this, 181, 47);
+      cockpitBack.addBox(-6F, -14F, -4F, 12, 9, 0);
+      cockpitBack.setRotationPoint(0F, 12F, 0F);
+      cockpitBack.setTextureSize(256, 256);
+      cockpitBack.mirror = true;
+      setRotation(cockpitBack, 0F, 3.141593F, 0F);
       topFin = new ModelRenderer(this, 47, 165);
       topFin.addBox(-0.5F, -24F, -7F, 1, 12, 8);
       topFin.setRotationPoint(0F, 12F, 0F);
       topFin.setTextureSize(256, 256);
       topFin.mirror = true;
       setRotation(topFin, 0.4363323F, 3.141593F, 0F);
-      engineTipLeft = new ModelRenderer(this, 0, 209);
-      engineTipLeft.addBox(-18.5F, -8.5F, -19F, 9, 9, 3);
-      engineTipLeft.setRotationPoint(0F, 12F, 0F);
-      engineTipLeft.setTextureSize(256, 256);
-      engineTipLeft.mirror = true;
-      setRotation(engineTipLeft, 0F, 3.141593F, 0F);
-      engineTipRight = new ModelRenderer(this, 26, 209);
-      engineTipRight.addBox(9.5F, -8.5F, -18.06667F, 9, 9, 3);
-      engineTipRight.setRotationPoint(0F, 12F, 0F);
-      engineTipRight.setTextureSize(256, 256);
-      engineTipRight.mirror = true;
-      setRotation(engineTipRight, 0F, 3.141593F, 0F);
   }
   
   public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
@@ -176,7 +185,9 @@ public class ModelBasicShip extends ModelBase
     chassisMain.render(f5);
     chassisBody.render(f5);
     engineRight.render(f5);
+    engineTipRight.render(f5);
     engineLeft.render(f5);
+    engineTipLeft.render(f5);
     wingLeft.render(f5);
     wingRight.render(f5);
     chassisDetailTop.render(f5);
@@ -188,13 +199,16 @@ public class ModelBasicShip extends ModelBase
     chassisNose2.render(f5);
     chassisNose3.render(f5);
     chassisBack.render(f5);
+    GL11.glPushMatrix();
+    GL11.glEnable(GL11.GL_BLEND);
     cockpitTop.render(f5);
     cockpitRight.render(f5);
     cockpitLeft.render(f5);
     cockpitFront.render(f5);
+    cockpitBack.render(f5);
+    GL11.glDisable(GL11.GL_BLEND);
+    GL11.glPopMatrix();
     topFin.render(f5);
-    engineTipLeft.render(f5);
-    engineTipRight.render(f5);
   }
   
   private void setRotation(ModelRenderer model, float x, float y, float z)
@@ -204,4 +218,3 @@ public class ModelBasicShip extends ModelBase
     model.rotateAngleZ = z;
   }
 }
-
